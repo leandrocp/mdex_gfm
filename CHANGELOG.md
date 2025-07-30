cd4b96037a80050d18f2276558d8b98814865c6c
Leandro Pereira <leandro@leandro.io>
2025-07-29 20:32:18 -0400

    v0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 0.1.0 - 2025-07-29
 
 ### Added
 - Add `MDExGFM.attach/2` attach the plugin to an `MDEx` instance
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MDExGFM.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/leandrocp/mdex_gfm"
-  @version "0.3.0"
+  @version "0.1.0"
 
   def project do
     [
